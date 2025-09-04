6e764bc94f3b33873c213cc0e8ac9d5e8d694732
Your Name <your@email.com>
2025-09-03 17:54:04 -0800

    paola

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-wrew
+def obtener_primer_elemento(lista):
+    if lista:
+        return lista[0]  # Siempre 1 operación
+    return None
+
+import time
+lista = [12,123,1234,12345]
+
+inicio = time.time()
+obtener_primer_elemento(lista)
+print(f"Tiempo de ejecucion = {time.time()-inicio}")
