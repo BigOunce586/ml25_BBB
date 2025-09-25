d46b97dd967c786052291e54938971bc6fceb796
Your Name <your@email.com>
2025-09-25 09:43:06 -0800

    m

--- src/ml25/datasets/customer_purchases/modcsv.py
+++ src/ml25/datasets/customer_purchases/modcsv.py
@@ -1,11 +1,19 @@
 import pandas as pd
 
-ruta_csv = r"C:\Users\paola\Downloads\ml25_BBB\src\ml25\datasets\customer_purchases\customer_purchases_train_modify.csv"
+ruta_csv = r"C:\Users\paola\Downloads\ml25_BBB\src\ml25\datasets\customer_purchases\customer_purchases_test_modify.csv"
 
 dataFrame = pd.read_csv(ruta_csv)
 
 # Modificar la columna 'color'
+dataFrame["color"] = dataFrame["color"].replace("imgb.jpg", 1)
+dataFrame["color"] = dataFrame["color"].replace("imgbl.jpg", 0.714)
+dataFrame["color"] = dataFrame["color"].replace("imgg.jpg", 0.429)
 dataFrame["color"] = dataFrame["color"].replace("imgp.jpg", -0.429)
+dataFrame["color"] = dataFrame["color"].replace("imgw.jpg", -0.714)
+dataFrame["color"] = dataFrame["color"].replace("imgo.jpg", 0.143)
+dataFrame["color"] = dataFrame["color"].replace("imgy.jpg",-1)
+dataFrame["color"] = dataFrame["color"].replace("imgr.jpg",-0.1)
+
 
 # Guardar en el mismo archivo
 dataFrame.to_csv(ruta_csv, index=False)
