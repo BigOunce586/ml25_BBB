dd67f9eea6d7f8fe56e3def591bb7100ee379eda
Your Name <your@email.com>
2025-09-26 20:57:01 -0800

    m

--- src/ml25/datasets/customer_purchases/modcsv_train.py
+++ src/ml25/datasets/customer_purchases/modcsv_train.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from datetime import datetime
+import re
+
 
 ruta_csv = r"C:\Users\paola\Downloads\ml25_BBB\src\ml25\datasets\customer_purchases\customer_purchases_train_modify.csv"
 
@@ -25,10 +27,51 @@ dataFrame["customer_gender"] = dataFrame["customer_gender"].fillna(3)
 #   lambda x: hoy.year - x.year - ((hoy.month, hoy.day) < (x.month, x.day)) if pd.notnull(x) else None
 #)
 
-#hoy2 = pd.Timestamp.now()
+#meses de sign up hoy2 = pd.Timestamp.now()
 #dataFrame["customer_signup_date"] = pd.to_datetime(dataFrame["customer_signup_date"], errors="coerce")
 #dataFrame["customer_signup_date"] = ((hoy2 - dataFrame["customer_signup_date"]).dt.days / 30.44).round(3) # 30.44 es el promedio de días por mes
 
+
+mapa = {
+    "jacket": 1.0,
+    "blouse": 0.6,
+    "jeans": 0.2,
+    "skirt": -0.2,
+    "shoes": -0.6,
+    "dress": -1.0
+}
+
+dataFrame["item_category"] = dataFrame["item_category"].map(mapa)
+dataFrame.to_csv("customer_purchases_train.csv", index=False)
+
+adjectives = [
+    "premium",
+    "casual",
+    "modern",
+    "exclusive",
+    "classic",
+    "stylish",
+    "elegant",
+    "lightweight",
+    "durable"
+]
+
+pattern = re.compile(r'\b(' + '|'.join(map(re.escape, adjectives)) + r')\b', flags=re.IGNORECASE)
+
+def keep_adjectives(title):
+    if pd.isna(title):
+        return ""      
+    matches = [m.group(0) for m in pattern.finditer(str(title))]
+    seen = set()
+    result = []
+    for m in matches:
+        key = m.lower()
+        if key not in seen:
+            seen.add(key)
+            result.append(m)   
+    return " ".join(result) 
+dataFrame = pd.read_csv(ruta_csv)
+dataFrame["item_title"] = dataFrame["item_title"].apply(keep_adjectives)
 
 
 dataFrame.to_csv(ruta_csv, index=False)
